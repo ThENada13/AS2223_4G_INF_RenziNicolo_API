c05efa06a8b6ea4eadf5a8bd86266fed05a7ea41
PC05 <PC05@INFO5-5376>
2023-03-16 08:14:53 +0100

    creazione controller

---
+++ AS2223_4G_INF_RenziNicolo_API/Controllers/ProvaVerifica.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_RenziNicolo_API.Controllers
+{
+    public class ProvaVerifica : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
