6ffc8b1c661e704caa494bd75e64d1af8c657a94
PC05 <PC05@INFO5-5376>
2023-03-16 08:33:30 +0100

    pari o dispari

---
+++ AS2223_4G_INF_RenziNicolo_API/Controllers/Prova.cs
@@ -0,0 +1,13 @@
+﻿namespace AS2223_4G_INF_RenziNicolo_API
+{
+    public class Prova
+    {
+        public int num { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
--- AS2223_4G_INF_RenziNicolo_API/Controllers/ProvaVerifica.cs
+++ AS2223_4G_INF_RenziNicolo_API/Controllers/ProvaVerifica.cs
@@ -8,5 +8,19 @@ namespace AS2223_4G_INF_RenziNicolo_API.Controllers
         {
             return View();
         }
+
+        [HttpGet(Name = "GetNumber")]
+        public JsonResult Get(int numero)
+        {
+            numero = numero % 2;
+            if (numero == 0)
+            {
+                return Json(new { status = "OK", message = "è pari"});
+            }
+            else
+            {
+                return Json(new { status = "OK", message = "è dispari" });
+            }
+        }
     }
 }
